9de0c0fde323cf0fb9f24eee7d74bd667b934996
ManuelLatorre98 <manuel.latorre@est.fi.uncoma.edu.ar>
2022-08-20 20:02:09 -0300

    print y creacion de conjuntos

--- src/TP1/MainPunto2.java
+++ src/TP1/MainPunto2.java
@@ -3,24 +3,16 @@ package TP1;
 public class MainPunto2 {
 
   public static void main(String[] args) {
-    int [] universe = {1,2,3,4,5,6,7,8,9,15}; //SUPONEMOS ORDENADO MENOR A MAYOR
+    int [] universe = {1,2,3,4,5,6,7,8,9,10}; //SUPONEMOS ORDENADO MENOR A MAYOR
     int maxElem= universe[(universe.length-1)];
     int [] set = new int [maxElem+1];
     configTag(set, universe);
-
-    int elem= 11;
-    //System.out.println("setTag of elem "+elem+": "+search(elem, set));
-
-    int elemA=1;
-    int elemB=3;
-    int tagA= search(elemA, set);
-    int tagB= search(elemB, set);
-    join(tagA, tagB,set);
-    join(tagA, 7,set);
-    for (int i = 0; i < universe.length; i++) {
-      System.out.println("setTag of elem "+universe[i]+": "+search(universe[i], set));
-    }
-
+    int [] set1= {1,3,7};
+    int [] set3= {2,5,9,10};
+    int [] set4= {6,8};
+    insertSet(set1, set);
+    insertSet(set3, set);
+    insertSet(set4, set);
     printSets(universe,set);
 
   }
@@ -61,18 +53,30 @@ public class MainPunto2 {
 
       if(sets[resultSearch]==null){
         sets[resultSearch]="";
-      }else{
-
       }
-
-      sets[resultSearch]+=","+universe[i];
+      if(sets[resultSearch].equals("")){
+        sets[resultSearch]+=universe[i];
+      }else{
+        sets[resultSearch]+=","+universe[i];
+      }
     }
 
     for (int i = 0; i < sets.length; i++) {
       if(sets[i]!=""){
-        sets[i]= "{"+sets[i]+"},";
+        sets[i]= "Set"+i+": {"+sets[i]+"}";
       }
       System.out.println(sets[i]);
     }
   }
+  /*recibe a ordered set of elements to be created*/
+  public static void insertSet(int [] elems, int[] set){
+    int elemA=elems[0]; //The min element is always the first
+    int tagA = search(elemA, set);
+    int tagB;
+    for (int i = 1; i < elems.length; i++) {
+      tagB=  search(elems[i], set);
+      join(tagA, tagB,set);
+    }
+  }
+
 }
