3e85b065693d041c7f4aa4920dc0931470f519ec
ManuelLatorre98 <manuel.latorre@est.fi.uncoma.edu.ar>
2022-11-25 16:44:51 -0300

    Diseño de algoritmos fin

--- src/Pruebas/M.java
+++ src/Pruebas/M.java
@@ -1,9 +1,30 @@
 package Pruebas;
 
 public class M {
-  int a = 1, b = 2;
-  static int c=14;
-  public void print() {
+    int a = 0, b = 0;
+    static int c=9;
+    int m1() {
+      int x = 11;
+      b = b - x;
+      c = c * 2;
+      System.out.println("En M a = " + a);
+      System.out.println(" b = " + b);
+      System.out.println(" c = " + c);
+      return 1;
+    }
+
+    int m2() {
+      c = c * 3;
+      System.out.println(++N.d);
+      return 4;
+    }
+
+    static void m3() {
+      System.out.println("En M c = " + c);
+    }
+
+
+  /*public void print() {
 
     System.out.println("CLASE M: " + a);
     m3();
@@ -11,5 +32,5 @@ public class M {
 
   public static void m3(){
     System.out.println("m3 de M");
-  }
+  }*/
 }
